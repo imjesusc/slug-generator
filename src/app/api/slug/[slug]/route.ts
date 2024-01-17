d95d20d60884f2a5736b55cc341f0ee87810082e
imjesusc <iamjesus.ds@gmail.com>
2024-01-17 18:18:23 -0500

    feat(Root): Added GET endPoint in slug api

---
+++ src/app/api/slug/[slug]/route.ts
@@ -0,0 +1,27 @@
+import { prisma } from "@/lib/prisma"
+import { Prisma } from "@prisma/client"
+import { NextResponse } from "next/server"
+
+export async function GET (request: Request, {params}: {params: { slug: string}}) {
+  const { slug } = params
+  const slugUrl = `/cs/${slug}`
+
+  
+  try {
+    const getSimpleSlug = await prisma.simpleShortenedUrl.findUnique({
+      where: { customSlug: slugUrl },
+    })
+
+    return NextResponse.json(getSimpleSlug)
+
+  } catch (error) {
+    if(error instanceof Prisma.PrismaClientKnownRequestError) {
+      console.log(error.message)
+      return NextResponse.json(
+        { message: error.message },
+        {status: 400}
+      )
+    }
+  }
+
+} 
