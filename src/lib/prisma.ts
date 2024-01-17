6ee834d5d18d1fcfd9e6f455b8062f7b126786b7
imjesusc <iamjesus.ds@gmail.com>
2024-01-17 12:30:11 -0500

    feat(Root): Update prisma client config

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,3 +1,12 @@
 import { PrismaClient } from '@prisma/client'
+import { PrismaLibSQL } from '@prisma/adapter-libsql'
+import { createClient } from '@libsql/client'
 
-export const prisma = new PrismaClient()
+const libsql = createClient({
+  // @ts-expect-error Description of why the error is expected and being ignored
+  url: process.env.TURSO_DATABASE_URL,
+  authToken: process.env.TURSO_AUTH_TOKEN
+})
+const adapter = new PrismaLibSQL(libsql)
+
+export const prisma = new PrismaClient({ adapter })
