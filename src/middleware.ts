d351887075537f927a78f9cf1a05f471e924c0c6
imjesusc <iamjesus.ds@gmail.com>
2024-01-18 17:38:04 -0500

    feat(Root): Added cors config

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,9 +6,10 @@ interface Data {
 }
 export async function middleware (request: NextRequest, ev: NextFetchEvent) {
   const slug = request.nextUrl.pathname.split('/').pop()
-
   const apiURL = request.nextUrl.origin
+
   const res = await fetch(`${apiURL}/api/slug/${slug}`)
+
   if (res.status === 404) {
     return NextResponse.redirect(request.nextUrl.origin)
   }
@@ -16,7 +17,7 @@ export async function middleware (request: NextRequest, ev: NextFetchEvent) {
   const data: Data = await res.json()
   console.log(data)
 
-  if (data?.originalUrl !== undefined) {
+  if (data?.originalUrl) {
     return NextResponse.redirect(data.originalUrl)
   }
 }
