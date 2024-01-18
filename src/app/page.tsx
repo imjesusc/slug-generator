d9221734fbbba28b0105c0d0325a0a8401c3e9d9
imjesusc <iamjesus.ds@gmail.com>
2024-01-18 00:42:13 -0500

    feat(Root): Added favicon and layout metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { SimpleSlugForm, SimpleSlugTable } from '@/components/organisms'
 export default function Home () {
   return (
   <main className='w-screen h-auto  my-10 laptop:mt-56 container grid gap-10 place-content-center'>
-    <h1 className='text-4xl text-balance font-bold'>Open Source Custom Link Slug Generator</h1>
+    <h1 className='text-4xl text-balance font-bold'>Open Source Custom Slug Generator</h1>
     <SimpleSlugForm />
     <SimpleSlugTable />
   </main>
