e223307a21534819bc1803e37bf329c67e311fc1
imjesusc <iamjesus.ds@gmail.com>
2024-01-18 09:36:36 -0500

    fix: prisma schema config env undefined

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -3,10 +3,10 @@ import { PrismaLibSQL } from '@prisma/adapter-libsql'
 import { createClient } from '@libsql/client'
 
 const libsql = createClient({
-  // @ts-expect-error Description of why the error is expected and being ignored
-  url: process.env.TURSO_DATABASE_URL,
-  authToken: process.env.TURSO_AUTH_TOKEN
+  url: `${process.env.TURSO_DATABASE_URL}`,
+  authToken: `${process.env.TURSO_AUTH_TOKEN}`
 })
+
 const adapter = new PrismaLibSQL(libsql)
 
 export const prisma = new PrismaClient({ adapter })
