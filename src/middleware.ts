b1d899b6c2d8a86bfad9fbd437eceeca47ba7036
imjesusc <iamjesus.ds@gmail.com>
2024-01-18 19:58:15 -0500

    Update fetch no-cors mode in middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -8,7 +8,7 @@ export async function middleware (request: NextRequest, ev: NextFetchEvent) {
   const slug = request.nextUrl.pathname.split('/').pop()
   const apiURL = request.nextUrl.origin
 
-  const res = await fetch(`${apiURL}/api/slug/${slug}`)
+  const res = await fetch(`${apiURL}/api/slug/${slug}`, { mode: 'no-cors' })
 
   if (res.status === 404) {
     return NextResponse.redirect(request.nextUrl.origin)
