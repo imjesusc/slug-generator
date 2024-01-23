cedc9a12e785cffe6282b4f78e0bd90238a5e7ec
imjesusc <iamjesus.ds@gmail.com>
2024-01-23 00:22:50 -0500

    refactor(Root): Update code indententation

--- src/components/molecules/SlugCard.tsx
+++ src/components/molecules/SlugCard.tsx
@@ -38,47 +38,65 @@ const handleDelete = async (userId: string | undefined, slugId: number | undefin
 export default function SlugCard ({ id, url, slug, description }: CustomSlugInterface) {
   const { data } = useSession()
   return (
-    <Card>
-      <CardHeader className="flex justify-between">
+    <Card className='hover:bg-accent transition-colors'>
+      <CardHeader className='flex justify-between'>
         <div className='flex justify-between gap-4'>
-        <div className='grid gap-1'>
-        <CardTitle>{slug}</CardTitle>
-        <CardDescription className='truncate'>{url}</CardDescription>
-        </div>
+          <div className='grid gap-1'>
+            <div className='flex items-center gap-2'>
+              <CardTitle>{slug}</CardTitle><CopyIcon className='w-4 h-4 cursor-pointer  active:scale-105' onClick={async () => {
+                await copyToClipboard(`http://localhost:3000/cs/${slug}`)
+              }} />
+            </div>
+            <CardDescription className='truncate'>{url}</CardDescription>
+          </div>
 
           <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-              <Button className='flex gap-2 h-10' variant='outline'>
+            <DropdownMenuTrigger asChild>
+              <Button className='flex gap-2 h-10 ' variant='outline'>
                 Controls
               </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent className="w-36" align="end" forceMount>
-            <DropdownMenuGroup className='font-sans'>
-              <DropdownMenuItem onClick={async () => { await copyToClipboard(`http://localhost:3000/cs/${slug}`) }}className='cursor-pointer'>
-                Copy
-                <DropdownMenuShortcut><CopyIcon /></DropdownMenuShortcut>
-              </DropdownMenuItem>
-              <ControlsForm
-              slugData={{ url, id, slug, description }}
-               action='Update'
-               >
-                <div className="relative flex hover:bg-accent items-center rounded-sm px-2 py-1.5 text-sm outline-none transition-colors focus:bg-accent focus:text-accent-foreground  cursor-pointer">Edit<span className="ml-auto text-xs tracking-widest opacity-60"><MixerHorizontalIcon /></span></div>
-               </ControlsForm>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className='w-36' align='end' forceMount>
+              <DropdownMenuGroup className='font-sans'>
+                <DropdownMenuItem
+                  onClick={async () => {
+                    await copyToClipboard(`http://localhost:3000/cs/${slug}`)
+                  }}
+                  className='cursor-pointer'
+                >
+                  Copy
+                  <DropdownMenuShortcut>
+                    <CopyIcon />
+                  </DropdownMenuShortcut>
+                </DropdownMenuItem>
+                <ControlsForm slugData={{ url, id, slug, description }} action='Update'>
+                  <div className='relative flex hover:bg-accent items-center rounded-sm px-2 py-1.5 text-sm outline-none transition-colors focus:bg-accent focus:text-accent-foreground  cursor-pointer'>
+                    Edit
+                    <span className='ml-auto text-xs tracking-widest opacity-60'>
+                      <MixerHorizontalIcon />
+                    </span>
+                  </div>
+                </ControlsForm>
 
-              <DropdownMenuItem className='cursor-pointer' onClick={async () => { await handleDelete(data?.userId, id) }}>
-                Delete
-                <DropdownMenuShortcut><ScissorsIcon /></DropdownMenuShortcut>
-              </DropdownMenuItem>
-            </DropdownMenuGroup>
-          </DropdownMenuContent>
-        </DropdownMenu>
+                <DropdownMenuItem
+                  className='cursor-pointer'
+                  onClick={async () => {
+                    await handleDelete(data?.userId, id)
+                  }}
+                >
+                  Delete
+                  <DropdownMenuShortcut>
+                    <ScissorsIcon />
+                  </DropdownMenuShortcut>
+                </DropdownMenuItem>
+              </DropdownMenuGroup>
+            </DropdownMenuContent>
+          </DropdownMenu>
         </div>
       </CardHeader>
 
       <CardContent>
-        <CardDescription className='h-[40px] max-h-[40px] overflow-hidden'>
-          {description}
-        </CardDescription>
+        <CardDescription className='h-[40px] max-h-[40px] overflow-hidden'>{description}</CardDescription>
       </CardContent>
     </Card>
   )
