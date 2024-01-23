5016c7294e3fef1c3c2f33672610f81ef8bc1910
imjesusc <iamjesus.ds@gmail.com>
2024-01-22 20:36:23 -0500

    feat: Added PUT endpoint in slugs api

--- src/app/api/slugs/route.ts
+++ src/app/api/slugs/route.ts
@@ -44,3 +44,45 @@ export async function POST (request: Request) {
     return NextResponse.json({ message: 'Something went wrong' }, { status: 500 })
   }
 }
+
+export async function PUT (request: Request) {
+  try {
+    const { url, slug, description, userId, id } = await request.json()
+
+    // Primero verificamos que el id del slug exista
+    const existingSlug = await prisma.link.findUnique({
+      where: {
+        id,
+        userId
+      }
+    })
+
+    if (!existingSlug) {
+      return NextResponse.json(
+        { message: 'Slug does not exist.' },
+        { status: 400 }
+      )
+    }
+
+    const createdSlug = await prisma.link.update({
+      where: {
+        id
+      },
+      data: {
+        url,
+        slug,
+        description,
+        userId
+      }
+    })
+
+    if (createdSlug) return NextResponse.json({ message: 'Slug updated successfully.' }, { status: 201 })
+  } catch (error) {
+    if (error instanceof Prisma.PrismaClientKnownRequestError) {
+      console.log(error.message)
+      return NextResponse.json({ message: error.message }, { status: 500 })
+    }
+
+    return NextResponse.json({ message: 'Something went wrong' }, { status: 500 })
+  }
+}
