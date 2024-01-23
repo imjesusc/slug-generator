67bc1b961ee068f7c48b8a330c9b37255630d996
imjesusc <iamjesus.ds@gmail.com>
2024-01-22 19:17:56 -0500

    Added NexAuth configuration for user ID recovery

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -10,7 +10,23 @@ export const handler = NextAuth({
       clientId: `${process.env.GITHUB_CLIENT_ID}`,
       clientSecret: `${process.env.GITHUB_SECRET}`
     })
-  ]
+  ],
+  callbacks: {
+    async session ({ session, user }) {
+      return await Promise.resolve({
+        ...session,
+        userId: user.id
+      })
+    }
+  }
 })
 
 export { handler as GET, handler as POST }
+declare module 'next-auth' {
+  /**
+   * Returned by `useSession`, `getSession` and received as a prop on the `Provider` React Context
+   */
+  interface Session {
+    userId: string
+  }
+}
