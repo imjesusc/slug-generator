09626008fbd2b11eed6109a9b2f61d4db45877df
imjesusc <iamjesus.ds@gmail.com>
2024-01-23 19:08:23 -0500

    feat: Update GET endpoint in slug api

--- src/app/api/slug/route.ts
+++ src/app/api/slug/route.ts
@@ -3,16 +3,41 @@ import { prisma } from '@/lib/prisma'
 import { Prisma } from '@prisma/client'
 
 export async function GET (request: Request) {
-  return NextResponse.json({ message: 'Hello World!' })
+  const url = new URL(request.url)
+  const customSlug = url.searchParams.get('slug')
+
+  if (!customSlug) return NextResponse.json({ message: 'Something went wrong. Slug not found' }, { status: 500 })
+
+  try {
+    const getSimpleSlug = await prisma.simpleShortenedUrl.findUnique({
+      where: {
+        customSlug
+      }
+    })
+
+    return NextResponse.json(getSimpleSlug)
+  } catch (error) {
+    if (error instanceof Prisma.PrismaClientKnownRequestError) {
+      console.log(error.message)
+      return NextResponse.json(
+        { message: error.message },
+        { status: 400 }
+      )
+    }
+
+    return NextResponse.json(
+      { message: 'Something went wrong.' },
+      { status: 500 }
+    )
+  }
 }
 
 export async function POST (request: Request) {
   const { originalUrl, customSlug } = await request.json()
-  const slugUrl = `/ss/${customSlug}`
 
   try {
     const existingSlug = await prisma.simpleShortenedUrl.findUnique({
-      where: { customSlug: slugUrl }
+      where: { customSlug }
     })
 
     if (existingSlug) {
@@ -25,15 +50,15 @@ export async function POST (request: Request) {
     await prisma.simpleShortenedUrl.create({
       data: {
         originalUrl,
-        customSlug: slugUrl
+        customSlug
       }
     })
 
     return NextResponse.json(
       {
         originalUrl,
-        url: `${request.headers.get('x-forwarded-proto')}://${request.headers.get('host')}/ss/${customSlug}`,
-        customSlug: slugUrl,
+        url: `${request.headers.get('x-forwarded-proto')}://${request.headers.get('host')}/${customSlug}`,
+        customSlug,
         message: 'Slug created successfully.'
       }
     )
