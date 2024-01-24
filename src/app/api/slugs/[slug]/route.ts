064e502afc733de94656b25810431e44ee4882ee
imjesusc <iamjesus.ds@gmail.com>
2024-01-23 19:10:00 -0500

    update get single slug GET endpoint in slugs api

--- src/app/api/slugs/[slug]/route.ts
+++ src/app/api/slugs/[slug]/route.ts
@@ -2,25 +2,18 @@ import { prisma } from '@/lib/prisma'
 import { Prisma } from '@prisma/client'
 import { NextResponse } from 'next/server'
 
-export async function GET (request: Request, { params }: { params: { id: string } }) {
-  const url = new URL(request.url)
-  const searchQuery = url.searchParams.get('search')
-  const { id } = params
-
-  if (!id) return NextResponse.json({ message: 'Something went wrong. Id not found' }, { status: 500 })
+export async function GET (request: Request, { params }: { params: { slug: string } }) {
+  const { slug } = params
+  if (!slug) return NextResponse.json({ message: 'Something went wrong. Id not found' }, { status: 500 })
 
   try {
-    const userSlugs = await prisma.link.findMany({
+    const userSlug = await prisma.link.findFirst({
       where: {
-        userId: id,
-        slug: {
-          contains: searchQuery || ''
-        }
+        slug
       }
-
     })
 
-    if (userSlugs) return NextResponse.json({ message: 'User slugs successfully retrieved', userSlugs }, { status: 200 })
+    if (userSlug) return NextResponse.json({ message: 'User slugs successfully retrieved', userSlug }, { status: 200 })
 
     return NextResponse.json({ message: 'Something went wrong.' }, { status: 500 })
   } catch (error) {
